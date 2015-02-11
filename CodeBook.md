730412becb11ef1a3bf21bb307d8f7cf5863ea56
bernardas78 <bernardasc@hotmail.com>
2015-02-11 11:06:39 +0200

    final commit

--- CodeBook.md
+++ CodeBook.md
@@ -1,6 +1,36 @@
-### The raw data
+### Code book
 
-It is critical that you include the rawest form of the data that you have access to. Here are some examples of the
-raw form of data:
+This code book describes the variables, the data, and any transformations or work that you performed to clean up the data
 
-* The strange 
+The raw data consists of the following:
+* Train and test data stored in separate directories /Train and /Test. Train contains ~7000 measurements and test contains ~3000
+* Each (train and test) data consists of:
+** Subjects of the measurements (who) in files [Train|Test]/subject_[train|test].txt. A vector of int, a numeric value 1:30 for each measurement
+** Activity indexes of the measurements (what kind of activity was performed) in files [Train|Test]/Y_[train|test].txt. A vector of int, a numeric value 1:6 for each measurement
+** Measurements in files [Train|Test]/X_[train|test].txt. It's a matrix of 561 columns (measures) where one row is one measurement of each measure. Values are normalized to range -1..1
+========
+** Files in directories [Train|Test]/Inertial Signals contain raw signal data. I did not use it for this assignment as per explanation https://class.coursera.org/getdata-011/forum/thread?thread_id=69
+
+Variables of raw data:
+** Activity labels in file activity_labels.txt. This is a mapping between activity index in [Train|Test]/Y_[train|test].txt and a readable activity name
+** Measure labels in file features.txt. This is a column headers vector for files [Train|Test]/X_[train|test].txt
+
+Tidy data set consists of the following columns:
+*subject - this a subject who measurement was performed on. Values 1:30
+*act_label - a readable activity name, character values one of 6 activities
+*var_name - a measure name (character value), a choice from file features.txt that contains "mean" or "std"
+*mean(var_value) - average of all measurements per subject per activity for a given measure
+
+Transformations performed to get from raw data to tidy data:
+*Train and test data was merged to a 3 data sets: subject, activity, measurements
+*Removed all columns from measurements data set other than the ones that contain "mean" or "std". Produced data set width was 79 columns
+*Merged 3 data sets (subject, activity, column-filtered measurements) to a single data set.
+*Pivoted the single data set to put 79 columns as rows (to make it more readable). This step produced a nice narrow data set (4 columns: subject,activity,measure name, measure value)
+*Aggregated the above data set by first 3 columns (subject,activity,measure name), and calculated mean for measure value.
+
+If you wish to run it yourself:
+*Unpack contents of https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip to a folder one above script run_analysis.R
+*Running it requires packages dplyr and tidyr - make sure you have them installed
+*set working directory where script run_analysis.R is
+*run script run_analysis.R. it takes a few min, most time is used to read data
+*resulting tidy data set is created in file tidyDS_task5.txt
--- run_analysis.R
+++ run_analysis.R
@@ -6,18 +6,18 @@
 #   5.From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
 
 #read data from files
-df_subject_train <- read.table("./UCI HAR Dataset/train/subject_train.txt")
-df_label_train <- read.table("./UCI HAR Dataset/train/Y_train.txt")
-df_data_train <- read.table("./UCI HAR Dataset/train/X_train.txt")
+df_subject_train <- read.table("../UCI HAR Dataset/train/subject_train.txt")
+df_label_train <- read.table("../UCI HAR Dataset/train/Y_train.txt")
+df_data_train <- read.table("../UCI HAR Dataset/train/X_train.txt")
 
-df_subject_test <- read.table("./UCI HAR Dataset/test/subject_test.txt")
-df_label_test <- read.table("./UCI HAR Dataset/test/Y_test.txt")
-df_data_test <- read.table("./UCI HAR Dataset/test/X_test.txt")
+df_subject_test <- read.table("../UCI HAR Dataset/test/subject_test.txt")
+df_label_test <- read.table("../UCI HAR Dataset/test/Y_test.txt")
+df_data_test <- read.table("../UCI HAR Dataset/test/X_test.txt")
 
-#1.merge data to a single set
+#1.merge data to 3 data sets: subject, activity label, measurements data
 #first, merge test and train sets by rows; then merge columns of all 3 data sets (subjects, labels, data) 
 #I am only including data from main directories (/train, /test), but excluding raw data from /Intertial Signals, 
-# , as per explanation https://class.coursera.org/getdata-011/forum/thread?thread_id=69
+# 
 df_subject<-rbind(df_subject_train,df_subject_test)
 df_label<-rbind(df_label_train,df_label_test)
 df_data<-rbind(df_data_train,df_data_test)
@@ -25,28 +25,25 @@ df_data<-rbind(df_data_train,df_data_test)
 #4.set column names which we will later refer to
 colnames(df_subject)<-c("subject") 
 colnames(df_label)<-c("act_index") 
-#column names of X_[test|train].txt come from features.txt
-col_names_X <- read.table("./UCI HAR Dataset/features.txt", col.names=c("ord","name"))
+col_names_X <- read.table("../UCI HAR Dataset/features.txt", col.names=c("ord","name")) #column names of X_[test|train].txt come from features.txt
 colnames(df_data)<-as.character(col_names_X$name)
 
-#finally, merge subject, activity, data to a single data set. this completes task 1
+#1.finally, merge subject, activity, data to a single data set. this completes task 1
 df <- cbind(df_subject, df_label, df_data)
 
-#2.extract only measurements that contain "mean" or "std"
-# first, read column names into a matrix col_names_X; then filter out columns that do not contain "mean" or "std"
+#2.extract only measurements for variables containing "mean" or "std". 
 col_rowNums_filtered <- c(grep("mean", col_names_X$name),grep("std", col_names_X$name)) #this is row numbers in file features.txt
 col_names_filtered <- as.character(col_names_X[col_rowNums_filtered,"name"])
-
-# preserve columns "subject" and "act_index"; remove all other columns not containing mean/std
-df<-df[c("subject","act_index",col_names_filtered)]
-
+df<-df[c("subject","act_index",col_names_filtered)] # preserve columns "subject" and "act_index"; remove all other columns not containing mean/std
 
 #3. Replace activity index (act_index) field with label from file activity_labels.txt
-activity_names <-read.table("./UCI HAR Dataset/activity_labels.txt", col.names=c("act_index","act_label"))
+activity_names <-read.table("../UCI HAR Dataset/activity_labels.txt", col.names=c("act_index","act_label"))
 df<-merge(activity_names,df)
 df$act_index <- NULL
 
-#5. 
+#5. Create a tidy narrow data set with means for each measure by subject and activity;
+# first, pivot the table to make each measure as row;
+# then summarize mean by subject, activity and measure name
 library(dplyr)
 library(tidyr)
 
@@ -55,5 +52,6 @@ tbl_task5_pivot <-gather(tbl_task5,var_name,var_value,-subject,-act_label)
 tbl_task5_pivot_grouped <- group_by(tbl_task5_pivot, subject,act_label,var_name)
 tbl_task5_summary <- summarize(tbl_task5_pivot_grouped,mean(var_value))
 
+#write out the resulting data set to a file
 write.table(tbl_task5_summary,"./tidyDS_task5.txt",row.names=FALSE)
 
